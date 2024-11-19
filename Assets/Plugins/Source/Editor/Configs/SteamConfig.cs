744ea6a8320bc6b753831de61c385b6bd21579fc
Paul Hazen <paul-hazen@live.com>
2024-11-18 23:44:42 -0800

    fix: Correct self-referential issue with SteamConfig.

--- Assets/Plugins/Source/Editor/Configs/SteamConfig.cs
+++ Assets/Plugins/Source/Editor/Configs/SteamConfig.cs
@@ -26,9 +26,9 @@ namespace PlayEveryWare.EpicOnlineServices
     using System.Collections.Generic;
     using Editor;
     using Editor.Utility;
+    using Newtonsoft.Json;
     using Utility;
 
-    [Serializable]
     [ConfigGroup("Steam Configuration")]
     // TODO: Make SteamConfig derive from EditorConfig, and update the native code
     //       to properly reference the correct file where appropriate.
@@ -54,6 +54,7 @@ namespace PlayEveryWare.EpicOnlineServices
         #endregion
 
         [ButtonField("Update from Steamworks.NET")]
+        [JsonIgnore]
         public Action UpdateFromSteamworksNET;
 
         static SteamConfig()
