5b5fdf91ac1faa78d1f7c80d88cfab4b91d02804
Jeff-ApexSystems <jeffrey.shyy@xa.epicgames.com>
2026-02-26 07:53:22 -0800

    Update lib/NativeCode/DynamicLibraryLoaderHelper/ManagedToUnmanagedBridge/AssemblyInfo.cpp
    Co-authored-by: Matt Clarke <matthew.clarke@mediatonic.co.uk>

--- lib/NativeCode/DynamicLibraryLoaderHelper/ManagedToUnmanagedBridge/AssemblyInfo.cpp
+++ lib/NativeCode/DynamicLibraryLoaderHelper/ManagedToUnmanagedBridge/AssemblyInfo.cpp
@@ -9,7 +9,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTitleAttribute(L"ManagedToUnmanagedBridge")];
 [assembly:AssemblyDescriptionAttribute(L"Makes some managed plugin code accessible from a native context.")];
 [assembly:AssemblyConfigurationAttribute(L"")];
-[assembly:AssemblyCompanyAttribute(L"2026 Epic Games Inc")];
+[assembly:AssemblyCompanyAttribute(L"Epic Games Inc")];
 [assembly:AssemblyProductAttribute(L"EOS Plugin for Unity")];
 [assembly:AssemblyCopyrightAttribute(L"Copyright © 2026")];
 [assembly:AssemblyTrademarkAttribute(L"")];
