7b3a55543a6762210619886bac6d3d2a11ebc8f3
Jskt02 <jessica.tello@xa.epicgames.com>
2026-05-04 11:20:38 -0600

    Update EOSManager_DynamicLoading.cs

--- com.playeveryware.eos/Runtime/Core/EOSManager_DynamicLoading.cs
+++ com.playeveryware.eos/Runtime/Core/EOSManager_DynamicLoading.cs
@@ -58,16 +58,14 @@ namespace PlayEveryWare.EpicOnlineServices
             public const string GfxPluginNativeRenderPath =
 #if UNITY_STANDALONE_OSX
                 "GfxPluginNativeRender-macOS";
-#elif UNITY_STANDALONE_WIN
-#if EOS_PLATFORM_WINDOWS_ARM64
-                "GfxPluginNativeRender-ARM64";
-#elif UNITY_64
-                "GfxPluginNativeRender-x64";
+#elif UNITY_STANDALONE_WIN && EOS_PLATFORM_WINDOWS_ARM64
+    "GfxPluginNativeRender-ARM64";
+#elif UNITY_STANDALONE_WIN && EOS_PLATFORM_WINDOWS_64
+    "GfxPluginNativeRender-x64";
+#elif UNITY_STANDALONE_WIN && EOS_PLATFORM_WINDOWS_32
+    "GfxPluginNativeRender-x86";
 #else
-                "GfxPluginNativeRender-x86";
-#endif // UNITY_64
-#else
-                #error Unknown platform
+                #error Unknown platform for GfxPluginNativeRenderPath
                 "GfxPluginNativeRender-unknown";
 #endif
 
