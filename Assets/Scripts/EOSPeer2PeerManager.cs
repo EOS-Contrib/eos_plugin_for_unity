cedc29744c697fe726288b4062b34cab0717042f
Matt Clarke <matthew.clarke@mediatonic.co.uk>
2026-06-18 12:20:13 +0100

    Redact socket name value in logging

--- Assets/Scripts/EOSPeer2PeerManager.cs
+++ Assets/Scripts/EOSPeer2PeerManager.cs
@@ -377,7 +377,7 @@ namespace PlayEveryWare.EpicOnlineServices.Samples
             else if (result == Result.Success)
             {
                 //Do something with chat output
-                Debug.LogFormat("Message received: peerId={0}, socketId={1}, data={2}", peerId.Redact(), socketId, Encoding.UTF8.GetString(data).Redact());
+                Debug.LogFormat("Message received: peerId={0}, socketId={1}, data={2}", peerId.Redact(), socketId.SocketName.Redact(), Encoding.UTF8.GetString(data).Redact());
 
                 if (!peerId.IsValid())
                 {
